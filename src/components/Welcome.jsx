9d2889b5d1902ce2a4181e15ad235170c1c5caa1
ahatwar822 <himanshugaupale875@gmail.com>
2026-01-15 18:18:04 +0530

    fix bugs in welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,8 +3,8 @@ import gsap from 'gsap'
 import React, { useRef } from 'react'
 
 const FONT_WEIGHTS = {
-    subtitle : { min: 100, max: 500, default: 100},
-    title: { min: 350, max: 900, default: 350 }
+    subtitle: { min: 100, max: 650, default: 100 },
+    title: { min: 350, max: 800, default: 350 }
 }
 const randerText = (text, className, baseWeight = 400) => {
     return [...text].map((char, i) => (
@@ -31,29 +31,49 @@ const setupTextHover = (container, text) => {
         });
     };
 
-    const handleMouesMove = (e) => {
-        const {left} = container.getBoundingClientRect();
-        const mouseX = e.clientX - left;
+const handleMouseMove = (e) => {
+        const containerRect = container.getBoundingClientRect();
+        const mouseX = e.clientX - containerRect.left;
 
-        letters.forEach((letter, i) => {
-            const {left:l , width: w} = letter.getBoundingClientRect();
-            const distance = Math.abs(mouseX - (l- left + w/2));
-            const intensity = Math.exp( -(distance **2)/ 20000);
+        letters.forEach((letter) => {
+            const rect = letter.getBoundingClientRect();
+            const letterCenter =
+                rect.left - containerRect.left + rect.width / 2;
 
-            animateLetter(letter, min + (max - min) * intensity);
+            const distance = Math.abs(mouseX - letterCenter);
+
+            // smooth + controlled falloff
+            const intensity = Math.exp(-(distance ** 2) / 12000);
+
+            gsap.to(letter, {
+                duration: 0.25,
+                ease: "power2.out",
+                fontVariationSettings: `'wght' ${
+                    min + (max - min) * intensity
+                }`,
+                y: -2 * intensity,
+                textShadow: `0px ${1 * intensity}px ${3 * intensity}px rgba(0,0,0,0.25)`
+            });
         });
     };
+
     const handleMouseLeave = () => {
-        letters.forEach((letter, i) => {
-            animateLetter(letter, base, 0.3);
+        letters.forEach((letter) => {
+            gsap.to(letter, {
+                duration: 0.35,
+                ease: "power2.out",
+                fontVariationSettings: `'wght' ${base}`,
+                y: 0,
+                textShadow: "0px 0px 0px rgba(0,0,0,0)"
+            });
         });
     };
     
-    container.addEventListener("mousemove", handleMouesMove);
+    container.addEventListener("mousemove", handleMouseMove);
     container.addEventListener("mouseleave", handleMouseLeave);
 
     return () => {
-        container.removeEventListener("mousemove", handleMouesMove);
+        container.removeEventListener("mousemove", handleMouseMove);
         container.removeEventListener("mouseleave", handleMouseLeave);
     };
 } 
@@ -74,10 +94,10 @@ const Welcome = () => {
     }, [])
     return (
         <section id='welcome'>
-            <p ref={subtitleRef} className='cursor-pointer'>
-                {randerText("Hey, I'am Aman! Welcome to my", 'text-3xl font-georama', 100)}
+            <p ref={subtitleRef} className='cursor-pointer text-gray-300 tracking-wide'>
+                {randerText("Hey, I'am Aman! Welcome to my", "text-3xl font-georama italic inline-block", 300)}
             </p>
-            <h1 ref={titleRef} className='mt-7 cursor-grab'>
+            <h1 ref={titleRef} className='mt-7 cursor-pointer'>
                 {randerText("Portfolio", 'text-9xl italic font-georama',400)}
             </h1>
 
